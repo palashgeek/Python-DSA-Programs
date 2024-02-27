03c4785af0c4a899dc98e81ac7550416ebdba70a
palashgeek <palashgupta7742@gmail.com>
2024-02-27 12:51:47 +0530

    [Palash] Add:Pyhton program to remove duplicate numberes in a list

---
+++ list/6_remove-duplicates.py
@@ -0,0 +1,7 @@
+def remove_duplicates(input_list):
+  
+    return list(set(input_list))
+
+input_list = input("Enter a list of elements separated by spaces: ").split()
+unique_list = remove_duplicates(input_list)
+print("List after removing duplicates:", unique_list)
