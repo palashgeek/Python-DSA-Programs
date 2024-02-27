f3277fe0adf109412d3a01d79d132b7cd821c7cf
palashgeek <palashgupta7742@gmail.com>
2024-02-27 12:20:09 +0530

    [Palash] Add: Python program to reverse a given string

---
+++ string/11_reverse-string.py
@@ -0,0 +1,6 @@
+def reverse_string(input_string):
+    return input_string[::-1]
+
+input_string = input("Enter a string: ")
+reversed_string = reverse_string(input_string)
+print("Reversed string:", reversed_string)
