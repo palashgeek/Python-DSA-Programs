4e2738c692217ea6247be0f5b18d7dfc4d006de2
palashgeek <palashgupta7742@gmail.com>
2024-02-27 12:09:47 +0530

    [PALASH] Add: Python program to find length of a string

---
+++ string/1_find-length.py
@@ -0,0 +1,7 @@
+def calculate_string_length(input_string):
+    length = len(input_string)
+    return length
+
+input_string = input("Enter a string: ")
+length = calculate_string_length(input_string)
+print("Length of the string:", length)
