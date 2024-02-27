b8d9997a5463366dc7c62cfd0f5096ce680cf073
palashgeek <palashgupta7742@gmail.com>
2024-02-27 12:55:46 +0530

    [Palash] Add:Pyhton program to append the one list into another

---
+++ list/13_append.py
@@ -0,0 +1,8 @@
+def append_list(list1, list2):
+    return list1 + list2
+
+
+list1 = [1, 2.0, 3]
+list2 = [4, 5, 6]
+appended_list = append_list(list1, list2)
+print("Appended list:", appended_list)
