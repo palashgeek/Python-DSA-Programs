4f6ef2801f33514515ab89ff933c5d840b0a5721
palashgeek <palashgupta7742@gmail.com>
2024-02-27 12:51:07 +0530

    [Palash] Add:Pyhton program to ccount the numbers in list with given condition in ques

---
+++ list/4_count.py
@@ -0,0 +1,8 @@
+def clone_list(input_list):
+    return list(input_list)
+
+
+input_list = [1, 2, 3, 4, 5]
+cloned_list = clone_list(input_list)
+print("Original list:", input_list)
+print("Cloned list:", cloned_list)
