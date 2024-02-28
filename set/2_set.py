3fbccf52ad3b7be793f48daf78d08cf0f8a343be
palashgeek <palashgupta7742@gmail.com>
2024-02-29 05:14:13 +0530

    [Palash] Add: python program to add membet toa set

---
+++ set/2_set.py
@@ -0,0 +1,16 @@
+'''
+
+@Author: Author Name
+
+@Date: 2021-02-11 18:00:30
+
+@Last Modified by: Author Name
+
+@Last Modified time: 2021-02-11 18:00:30
+
+@Title : add member to set.
+'''
+my_set = {1,2,3}
+my_set.add(4)
+my_set.update([5,6])
+print(my_set)
