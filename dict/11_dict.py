7649cd8d43c21c534021de513c6a3b062e5c7e6e
palashgeek <palashgupta7742@gmail.com>
2024-02-29 01:36:52 +0530

    [Palash] Add:Python program to convert a list into nested dict

---
+++ dict/11_dict.py
@@ -0,0 +1,12 @@
+'''
+
+@Author: Palash Gupta
+
+@Date: 2024-02-27 17:15:00
+
+@Last Modified by: Palash gupta
+
+@Last Modified time: 2024-02-27 17:15:00
+
+@Title :Write a Python program to convert a list into a nested dictionary of keys.
+'''
