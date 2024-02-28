bdb9b0fc672f616f88ec64cda8c6ee056d1857ec
palashgeek <palashgupta7742@gmail.com>
2024-02-28 15:51:43 +0530

    [Palash] Add: Python program to update the dict using key

---
+++ dict/2_dict.py
@@ -0,0 +1,24 @@
+'''
+
+@Author: Palash Gupta
+
+@Date: 2024-02-27 17:15:00
+
+@Last Modified by: Palash gupta
+
+@Last Modified time: 2024-02-27 17:15:00
+
+@Title :Write a Python script to add a key to a dictionary.
+'''
+
+
+if __name__ == "__main__":
+    original_dict = {0:10, 1:20}
+    print("Original dictionary is this:", original_dict)
+
+    original_dict[2] = 30
+    original_dict[3] = 40
+
+    print("updated dictionary is this:", original_dict)
+    
+
