60d2d828c5b1eb31b10bc80c6a71f35de4e7decb
palashgeek <palashgupta7742@gmail.com>
2024-02-29 01:03:50 +0530

    [Palash]Add:Python program to create a table from dic

---
+++ dict/9_dict.py
@@ -0,0 +1,14 @@
+'''
+
+@Author: Palash Gupta
+
+@Date: 2024-02-27 17:15:00
+
+@Last Modified by: Palash gupta
+
+@Last Modified time: 2024-02-27 17:15:00
+
+@Title :Write a Python program to create a dictionary from a string.
+Note: Track the count of the letters from the string.
+Sample string : 'w3resource'
+Expected output: Write a Python program to print a dictionary in table format.'''
