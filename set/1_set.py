4dc11fa4717ee809b5e3f24043799d78cd5eeb19
palashgeek <palashgupta7742@gmail.com>
2024-02-29 05:10:59 +0530

    [Palash] Add: python program to create a set

---
+++ set/1_set.py
@@ -0,0 +1,15 @@
+'''
+
+@Author: Author Name
+
+@Date: 2021-02-11 18:00:30
+
+@Last Modified by: Author Name
+
+@Last Modified time: 2021-02-11 18:00:30
+
+@Title : Write a Python program to create a set.
+
+'''
+my_set = {1,2,3,4,5}
+print(my_set)
