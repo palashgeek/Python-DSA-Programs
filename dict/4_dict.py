218c60e2b37da22cfa4646f4b3549441284af0f7
palashgeek <palashgupta7742@gmail.com>
2024-02-28 17:22:19 +0530

    [Palash]Add:Python program iterating over dictionary usinf for loops

---
+++ dict/4_dict.py
@@ -0,0 +1,17 @@
+
+my_dict = {'name': 'Alice', 'age': 30, 'city': 'New York'}
+
+
+print("Iterating over keys:")
+for key in my_dict:
+    print(key)
+
+
+print("\nIterating over values:")
+for value in my_dict.values():
+    print(value)
+
+
+print("\nIterating over key-value pairs:")
+for key, value in my_dict.items():
+    print(key, ":", value)
