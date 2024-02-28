d53d945008aa4362b664838ff12b960243851483
palashgeek <palashgupta7742@gmail.com>
2024-02-29 05:18:26 +0530

    [Palash] Add: python program to intersect  a set

---
+++ set/5_set.py
@@ -0,0 +1,17 @@
+'''
+
+@Author: Author Name
+
+@Date: 2021-02-11 18:00:30
+
+@Last Modified by: Author Name
+
+@Last Modified time: 2021-02-11 18:00:30
+
+@Title : intersection of a set.
+'''
+
+set1 = {1, 2, 3, 4}
+set2 = {3, 4, 5, 6}
+intersection_set = set1.intersection(set2)
+print("Intersection Set:", intersection_set)
