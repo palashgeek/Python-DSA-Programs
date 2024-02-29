e1ba86eb89fdfbab38152d9715655088d629357f
palashgeek <palashgupta7742@gmail.com>
2024-02-29 10:47:27 +0530

    [Palash] Add: python program to find defference of two sets

---
+++ set/8_set.py
@@ -0,0 +1,20 @@
+'''
+@Author: Author Name
+
+@Date: 2021-02-11 18:00:30
+
+@Last Modified by: Author Name
+
+@Last Modified time: 2021-02-11 18:00:30
+
+@Title : difference of a set.
+'''
+
+set1 = {1, 2, 3, 4, 5}
+set2 = {3, 4, 5, 6, 7}
+
+difference_set = set1.difference(set2)
+difference_set2 = set1.difference(set2)
+
+print("Set Difference (Using difference() method):", difference_set)
+print("Set Difference (Using - method):", difference_set2)
