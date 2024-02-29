596f349ffcd69b22936bec7c59f4f3b99ac7b573
palashgeek <palashgupta7742@gmail.com>
2024-02-29 10:48:29 +0530

    [Palash] Add: python program to clear the sets

---
+++ set/10_set.py
@@ -0,0 +1,16 @@
+'''
+@Author: Author Name
+
+@Date: 2021-02-11 18:00:30
+
+@Last Modified by: Author Name
+
+@Last Modified time: 2021-02-11 18:00:30
+
+@Title : clear a set.
+'''
+my_set = {1, 2, 3, 4, 5}
+
+my_set.clear()
+
+print("Set after clearing:", my_set)
