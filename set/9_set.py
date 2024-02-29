5a03b9496bdbf3c7ebca9f74659d875fb6033556
palashgeek <palashgupta7742@gmail.com>
2024-02-29 10:47:58 +0530

    [Palash] Add: python program to find symmetric  difference of two sets

---
+++ set/9_set.py
@@ -0,0 +1,19 @@
+'''
+@Author: Author Name
+
+@Date: 2021-02-11 18:00:30
+
+@Last Modified by: Author Name
+
+@Last Modified time: 2021-02-11 18:00:30
+
+@Title : symmetric difference of a set.
+'''
+
+set1 = {1, 2, 3, 4, 5}
+set2 = {3, 4, 5, 6, 7}
+
+symmetric_difference_set = set1.symmetric_difference(set2)
+
+print("Symmetric Difference (Using symmetric_difference() method):", symmetric_difference_set)
+#uncommon elements
