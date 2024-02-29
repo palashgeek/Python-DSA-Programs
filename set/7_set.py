21dcdffc4b12a0e474bf31050876e7338cdc9a00
palashgeek <palashgupta7742@gmail.com>
2024-02-29 10:37:09 +0530

    [Palash] Add: python program to union of two sets

---
+++ set/7_set.py
@@ -0,0 +1,19 @@
+'''
+
+@Author: Author Name
+
+@Date: 2021-02-11 18:00:30
+
+@Last Modified by: Author Name
+
+@Last Modified time: 2021-02-11 18:00:30
+
+@Title : union of a set.
+'''
+set1 = {1, 2, 3}
+set2 = {3, 4, 5}
+
+union_set = set1.union(set2)
+
+
+print("Union Set (Using union() method):", union_set)
