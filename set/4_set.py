fb5330d95c850f8e962d1cba1cb75a0a61662e9b
palashgeek <palashgupta7742@gmail.com>
2024-02-29 10:34:34 +0530

    [Palash] Add: python program to iterate over a set

---
+++ set/4_set.py
@@ -0,0 +1,17 @@
+'''
+
+@Author: Author Name
+
+@Date: 2021-02-11 18:00:30
+
+@Last Modified by: Author Name
+
+@Last Modified time: 2021-02-11 18:00:30
+
+@Title : iteration over a set.
+'''
+
+my_set = {1, 2, 3, 4, 5}
+
+for element in my_set:
+    print(element)
