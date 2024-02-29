fc0bec0b85c00a34211e2854ed0602c32d950b97
palashgeek <palashgupta7742@gmail.com>
2024-02-29 10:49:24 +0530

    [Palash] Add: python program to finf the max and min value of sets

---
+++ set/12_set.py
@@ -0,0 +1,20 @@
+'''
+@Author: Author Name
+
+@Date: 2021-02-11 18:00:30
+
+@Last Modified by: Author Name
+
+@Last Modified time: 2021-02-11 18:00:30
+
+@Title : max min of a set
+
+'''
+
+my_set = {3, 1, 4, 2, 5}
+
+max_value = max(my_set)
+min_value = min(my_set)
+
+print("Maximum value:", max_value)
+print("Minimum value:", min_value)
